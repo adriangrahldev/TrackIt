a342a97813ec803746608e780447d93d1d48c8b7
Adrian Grahl <adriangrahlmaciel@gmail.com>
2023-12-16 03:29:58 -0300

    Inicio del cronometro

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var isJobInput = document.getElementById("isJobInput");
 var hourPriceInput = document.getElementById("hourPrice");
 var jobForm = document.querySelector("#jobForm");
 var activityControl = document.querySelector('#activityControl');
+var startButton = document.getElementById('startButton');
 
 isJobInput.addEventListener("change", function (event) {
   if (isJobInput.checked) {
@@ -19,6 +20,12 @@ isJobInput.addEventListener("change", function (event) {
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   crearActividad();
+});
+
+startButton.addEventListener('click', function(){
+  if(actividadActual){
+    iniciarCronometro(actividadActual);
+  }
 });
 
 var actividades = [];
@@ -37,24 +44,39 @@ function crearActividad() {
   };
   console.log(actividad);
   setActividad(actividad);
+  form.reset()
+  form.parentNode.style.display = "none";
+  document.getElementById('activityName').innerText = actividad.nombre;
+  document.getElementById('activityTime').innerText = "0";
+  document.getElementById('activityPrice').innerText = "$"+actividad.precioHora;
+  document.getElementById('activityTotal').innerText = "$0";
+  if(!actividad.trabajo){
+    document.getElementById('activityPrice').style.display = "none";
+    document.getElementById('activityTotal').style.display = "none";
+  }else{
+    document.getElementById('activityPrice').style.display = "inline-block";
+    document.getElementById('activityTotal').style.display = "inline-block";
+  }
   // iniciarCronometro(actividad);
 }
 
 function iniciarCronometro(actividad) {
   actividad.tiempoInicio = new Date();
+  document.getElementById('startButton').classList.remove('active');
+  document.getElementById('stopButton').classList.add('active');
   actividad.timeoutObj = setInterval(actualizarCronometro, 1000);
 }
 
+
+
 function actualizarCronometro() {
   actividadActual.tiempo++;
-  calcularCosto(actividadActual);
-  console.log(
-    actividadActual.nombre,
-    " -> ",
-    actividadActual.tiempo,
-    " -> ",
-    actividadActual.costo
-  );
+
+  if(actividadActual.trabajo) {
+    calcularCosto(actividadActual);
+    document.getElementById('activityTotal').innerText = "$"+(actividadActual.tiempo*(actividadActual.precioHora/3600)).toFixed(2);
+  }
+  document.getElementById('activityTime').innerText = actividadActual.tiempo;
 }
 
 function detenerCronometro() {
